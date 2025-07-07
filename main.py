024e68e1f6d0a40ca69f65cef4b8cadb2ea0caaa
Pedro I <pedroizumida@outlook.com>
2025-07-07 10:21:55 -0300

    fixed main functions calls

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import datetime
 from collections import Counter
 import json
 
+
 def file_fill(file, header, message):
 	file.write(header)
 	if message is None: traceback.print_exc(file=file)
@@ -19,6 +20,8 @@ def file_fill(file, header, message):
 
 
 def log(filename,header,period,message=None,overright_size=2):
+	print(filename)
+
 	filename = filename + ".txt"
 	header = f"-=-=-= {header}: {period};  =-=-=-=\n\n"
 	
@@ -32,7 +35,7 @@ def log(filename,header,period,message=None,overright_size=2):
 			file_fill(file=f,header=header,message=message)
 
 
-def show_time(on,message,time):
+def duration(on,message,time):
 	if on: print(f"{message}: {time}")
 
 
@@ -40,9 +43,9 @@ def debug_code(debug,message,var=None):
 	if debug == 1: print(f"{message}: {var};\r\n")
 
 
-def extract(query,config,debug):
+def extract(query,db_config,debug=False):
 	try: 
-		connection = mysql.connector.connect(**config)
+		connection = mysql.connector.connect(**db_config)
 		cursor = connection.cursor()    
 
 		for result in cursor.execute(query, multi=True):
@@ -82,7 +85,7 @@ def result_set_transform(result_set,column_name,debug):
 	return(df)
 
 
-def base_generate(separated,query,config,debug,destination,basefile_name,day=0,last_month=0):
+def base_generate(separated,query,db_config,debug,destination,basefile_name,day=0,last_month=0):
 	column_name = database_column(query)
 
 	end_date = datetime.datetime.now() + datetime.timedelta(days=day)
@@ -92,58 +95,58 @@ def base_generate(separated,query,config,debug,destination,basefile_name,day=0,l
 	else: start_date = end_date - datetime.timedelta(days=today-1)
 
 	if separated == False:
-			result_set = extract(query,config,debug)
-			debug_code(debug,"Executed query",query)
-			df = result_set_transform(result_set,column_name,debug)
+		result_set = extract(query=query,db_config=db_config,debug=debug)
+		debug_code(debug,"Executed query",query)
+		df = result_set_transform(result_set,column_name,debug)
 
-			prefix = end_date.strftime("%Y%m%d")
-			
-			for path in destination:
-				if not os.path.exists(os.path.join(path,end_date.strftime('%Y'))): 
-					os.makedirs(os.path.join(path,end_date.strftime('%Y')))
+		prefix = end_date.strftime("%Y%m%d")
+		
+		for path in destination:
+			if not os.path.exists(os.path.join(path,end_date.strftime('%Y'))): 
+				os.makedirs(os.path.join(path,end_date.strftime('%Y')))
 
-				path_file = os.path.join(path,end_date.strftime('%Y'),f"{prefix}_{basefile_name}.xlsx")
-				debug_code(debug,"Previous month file name",path_file)
-				df.to_excel(path_file,index=False)
+			path_file = os.path.join(path,end_date.strftime('%Y'),f"{prefix}_{basefile_name}.xlsx")
+			debug_code(debug,"Previous month file name",path_file)
+			df.to_excel(path_file,index=False)
 	else:
-			temp_end_date = end_date + datetime.timedelta(days=day)
-			for i in range(last_month):
-				sql_q = query
+		temp_end_date = end_date + datetime.timedelta(days=day)
+		for i in range(last_month):
+			sql_q = query
 
-				for i in range((Counter(query.split())["custom"])+1):
-					if i % 2 == 0: sql_q = sql_q.replace("custom",start_date.strftime("%Y%m%d"),1)
-					else:  sql_q = sql_q.replace("custom",temp_end_date.strftime("%Y%m%d"),1)
+			for i in range((Counter(query.split())["custom"])+1):
+				if i % 2 == 0: sql_q = sql_q.replace("custom",start_date.strftime("%Y%m%d"),1)
+				else:  sql_q = sql_q.replace("custom",temp_end_date.strftime("%Y%m%d"),1)
 
-				result_set = extract(sql_q,debug)
+			result_set = extract(query=sql_q,db_config=db_config,debug=debug)
 
-				if result_set is not None and result_set != []:
-					df = result_set_transform(result_set,column_name,debug)
-							
-					debug_code(debug,"Previous month query", sql_q)
-					prefix = f"{start_date.strftime("%Y")}{start_date.strftime("%m").zfill(2)}.{start_date.strftime("%B")}"
+			if result_set is not None and result_set != []:
+				df = result_set_transform(result_set,column_name,debug)
+						
+				debug_code(debug,"Previous month query", sql_q)
+				prefix = f"{start_date.strftime("%Y")}{start_date.strftime("%m").zfill(2)}"
+				
+				for path in destination:
+					if not os.path.exists(os.path.join(path,start_date.strftime('%Y'))): 
+						os.makedirs(os.path.join(path,start_date.strftime('%Y')))
 					
-					for path in destination:
-						if not os.path.exists(os.path.join(path,start_date.strftime('%Y'))): 
-							os.makedirs(os.path.join(path,start_date.strftime('%Y')))
-						
-						path_file = os.path.join(path,start_date.strftime('%Y'),f"{prefix}_{basefile_name}.xlsx")
-						debug_code(debug,"Previous month file name",path_file)
-						df.to_excel(path_file,index=False)
-						debug_code(debug,"File created!!")
+					path_file = os.path.join(path,start_date.strftime('%Y'),f"{prefix}_{basefile_name}.xlsx")
+					debug_code(debug,"Previous month file name",path_file)
+					df.to_excel(path_file,index=False)
+					debug_code(debug,"File created!!")
 
-				end_date = end_date - datetime.timedelta(days=int(end_date.strftime('%d')))
-				temp_end_date = end_date + datetime.timedelta(days=day)
-						
-				debug_code(debug,"Previous month end_date",end_date)
+			end_date = end_date - datetime.timedelta(days=int(end_date.strftime('%d')))
+			temp_end_date = end_date + datetime.timedelta(days=day)
+					
+			debug_code(debug,"Previous month end_date",end_date)
 
-				start_date = end_date - datetime.timedelta(days=int(end_date.strftime('%d'))-1)
-				debug_code(debug,"Previous month start_date",start_date)
+			start_date = end_date - datetime.timedelta(days=int(end_date.strftime('%d'))-1)
+			debug_code(debug,"Previous month start_date",start_date)
 
 
 def main():
 	try:
 		config = configparser.RawConfigParser()
-		config.read(os.path.join(os.getcwd(),'config.ini'))
+		config.read(os.path.join(os.getcwd(),"config/config.ini"))
 		with open(os.path.join(os.getcwd(),"config/config.json")) as jsonfile: config_json = json.load(jsonfile)
 		debug = int(config["EVALUATE"]["debug"])
 		start = datetime.datetime.now()
@@ -155,21 +158,21 @@ def main():
 		with open(os.path.join(os.getcwd(),f"query/{config["FILE"]["name_query"]}.sql"), 'r', encoding='utf-8') as file: query = file.read()
 
 		match export_type:
-			case 0:
-				base_generate(separated=False,query=query,debug=debug,destination=destination,basefile_name=basefile_name)
-			case 1:  
-				base_generate(separated=True,query=query,debug=debug,destination=destination,basefile_name=basefile_name,day=day,last_month=int(config["FILE"]["previous_month"]))
+			case 0: 
+				base_generate(separated=False,query=query,db_config=config_json,debug=debug,destination=destination,basefile_name=basefile_name)
+			case 1: 
+				base_generate(separated=True,query=query,db_config=config_json,debug=debug,destination=destination,basefile_name=basefile_name,day=day,last_month=int(config["FILE"]["previous_month"]))
 			case 2:
 				index_where =  query.index("WHERE")
 				query=(query[:index_where-1]+";")
-				base_generate(separated=False,query=query,debug=debug,destination=destination,basefile_name=basefile_name,day=day,last_month=int(config["FILE"]["previous_month"]))
+				base_generate(separated=False,query=query,db_config=config_json,debug=debug,destination=destination,basefile_name=basefile_name,day=day,last_month=int(config["FILE"]["previous_month"]))
 		
 	except Exception as error:
 		debug_code(debug,({"eror": f"Failed to generate file(s): {error}"}))
 		log("log_error","'Saving base file'",datetime.datetime.today())
 
-	log(check_time,"Start Time",start)
-	log(check_time,"Duration",datetime.datetime.now()-start)
+	duration(check_time,"Start Time",start)
+	duration(check_time,"Duration",datetime.datetime.now()-start)
 
 
 if __name__ == "__main__":
